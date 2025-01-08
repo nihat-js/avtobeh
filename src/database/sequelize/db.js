eb56deb2bf46385dd0786aa9d7b3412f6a4eb51b
Nihat <abdullazadenihat@gmail.com>
2025-01-08 20:56:42 +0400

    fix

--- src/database/sequelize/db.js
+++ src/database/sequelize/db.js
@@ -1,22 +1,28 @@
 import { Sequelize } from 'sequelize';
 import mysql2 from 'mysql2'; // Import mysql2 explicitly
+import { config } from '@/src/lib/config';
 
-export const sequelize = new Sequelize('avtobeh_development', 'nihat', 'Salam3169!!', {
-  host: '84.247.20.175',
-  dialect: 'mysql',
-  dialectModule: mysql2,
-  benchmark: true,
-  freezeTableName: true,
-  logging: false,
-  dialectOptions: {
-    connectTimeout: 10000
-  },
-  pool: {
-    max: 5, // Adjust as necessary
-    min: 0,
-    idle: 10000 // Adjust as necessary
-  }
-});
+export const sequelize = new Sequelize(
+  config.databaseName,
+  config.databaseUser,
+  config.databasePassword,
+  {
+    host: config.databaseHost,
+    port: config.databasePort,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    benchmark: true,
+    freezeTableName: true,
+    logging: false,
+    dialectOptions: {
+      connectTimeout: 10000
+    },
+    pool: {
+      max: 5, // Adjust as necessary
+      min: 0,
+      idle: 10000 // Adjust as necessary
+    }
+  });
 
 
 
--- src/lib/config.js
+++ src/lib/config.js
@@ -2,4 +2,9 @@ import "server-only"
 
 export const config = {
   JWTSecret: process.env.JWT_SECRET || 'avtobeh-2025-jwt-jwt',
+  databaseHost: process.env.DATABASE_HOST || 'localhost',
+  databasePort: process.env.DATABASE_PORT || '3306',
+  databaseUser: process.env.DATABASE_USER || 'root',
+  databasePassword: process.env.DATABASE_PASSWORD || '',
+  databaseName: process.env.DATABASE_NAME || 'avto-beh',
 };
